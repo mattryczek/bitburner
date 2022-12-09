c86f794bb53e4619975c24bd1389f1c163d5b308
Mateusz Ryczek <skyfused@gmail.com>
2022-12-08 23:18:43 -0600

    Refactor overlord to use run() instead of importing

--- helpers/get_stats.js
+++ helpers/get_stats.js
@@ -1,25 +1,27 @@
 /** @param {NS} ns */
 
-export default function get_stats(ns, server) {
+export async function main(ns) {
+	let target = ns.args[0];
+	
 	let server_funds, sec, level, ram_used, ram_total, ports = 0;
 	let root = false;
 
-	root = ns.hasRootAccess(server);
-	ram_used = ns.getServerUsedRam(server);
-	ram_total = ns.getServerMaxRam(server);
-	server_funds = ns.getServerMoneyAvailable(server);
-	sec = ns.getServerRequiredHackingLevel(server);
-	level = ns.getServerSecurityLevel(server);
-	ports = ns.getServerNumPortsRequired(server);
+	root = ns.hasRootAccess(target);
+	ram_used = ns.getServerUsedRam(target);
+	ram_total = ns.getServerMaxRam(target);
+	server_funds = ns.getServerMoneyAvailable(target);
+	sec = ns.getServerRequiredHackingLevel(target);
+	level = ns.getServerSecurityLevel(target);
+	ports = ns.getServerNumPortsRequired(target);
 
-	ns.tprint('         __' + '_'.repeat(server.toString().length) + '__');
-	ns.tprint(`         >>${server}<<`);
-	ns.tprint('         ‾‾' + '‾'.repeat(server.toString().length) + '‾‾');
+	ns.tprint('         __' + '_'.repeat(target.toString().length) + '__');
+	ns.tprint(`         >>${target}<<`);
+	ns.tprint('         ‾‾' + '‾'.repeat(target.toString().length) + '‾‾');
 
 	ns.tprint(`Root status: ${root}`);
 	ns.tprint(`Number of ports to NUKE: ${ports}`);
 	ns.tprint(`RAM Usage: ${ram_used}GB / ${ram_total}GB`)
-	ns.tprint(`Current funds: \$${Math.trunc(server_funds).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} `);
-	ns.tprint(`Required Hack Level: ${sec} `);
+	ns.tprint(`Current funds: \$${Math.trunc(server_funds).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`);
+	ns.tprint(`Required Hack Level: ${sec}`);
 	ns.tprint(`Current Security Level: ${level.toFixed(4)}\n\n`);
 }
--- overlord.js
+++ overlord.js
@@ -1,49 +1,57 @@
 /** @param {NS} ns **/
 
-import get_stats from "./helpers/get_stats.js";
-import hack_server from "./helpers/hack_server.js";
-import weaken_server from "./helpers/weaken_server.js";
-import propagate from "./helpers/propagate.js";
-
+// import get_stats from "./helpers/get_stats.js";
+// import hack_server from "./helpers/hack_server.js";
+// import weaken_server from "./helpers/weaken_server.js";
+// import propagate from "./helpers/propagate.js";
+// import root_server from "./smart_root.js";
 
 export async function main(ns) {
-	const port_force = 1;
-
-	switch (ns.args[0]) {
-		case 'recon':
-			recon(ns);
-			break;
-		case 'weaken':
-			await weaken_server(ns, ns.args[1]);
-			break;
-		case 'hack':
-			await hack_server(ns, ns.args[1]);
-			break;
-		case 'stats':
-			get_stats(ns, ns.args[1]);
-			break;
-		case 'money':
-			get_money(ns);
-			break;
-		case 'propagate':
-			propagate(ns);
-			break;
-		default:
-			ns.tprint('I need to know what you want me to do...');
-	}
+  switch (ns.args[0]) {
+    case "recon":
+      recon(ns);
+      break;
+    case "weaken":
+      await weaken_server(ns, ns.args[1]);
+      break;
+    case "hack":
+      await hack_server(ns, ns.args[1]);
+      break;
+    case "stats":
+      get_stats(ns, ns.args[1]);
+      break;
+    case "money":
+      get_money(ns);
+      break;
+    case "propagate":
+      propagate(ns);
+      break;
+    case "nuke":
+      root_server(ns, ns.args[1]);
+      break;
+    default:
+      ns.tprint("I need to know what you want me to do...");
+  }
 }
 
 function recon(ns) {
-	for (let server of ns.scan()) {
-		get_stats(ns, server);
-	}
+  for (let server of ns.scan()) {
+    ns.run("helpers/get_stats.js", 1, server);
+  }
 }
 
 function get_money(ns) {
-	let server_funds = 0;
+  let server_funds = 0;
 
-	for (let server of ns.scan()) {
-		server_funds = ns.getServerMoneyAvailable(server);
-		ns.tprint(`${server}: \$${Math.trunc(server_funds).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`);
-	}
+  for (let server of ns.scan()) {
+    server_funds = ns.getServerMoneyAvailable(server);
+    ns.tprint(
+      `${server}: \$${
+        Math.trunc(server_funds).toString().replace(
+          /\B(?=(\d{3})+(?!\d))/g,
+          ",",
+        )
+      }`,
+    );
+  }
 }
