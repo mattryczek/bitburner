202c53563156443511c6a7051a43cb280b268f36
Mateusz Ryczek <skyfused@gmail.com>
2022-12-09 00:04:35 -0600

    Refactor rest of overlord.js

--- overlord.js
+++ overlord.js
@@ -1,10 +1,6 @@
 /** @param {NS} ns **/
 
-// import get_stats from "./helpers/get_stats.js";
-// import hack_server from "./helpers/hack_server.js";
-// import weaken_server from "./helpers/weaken_server.js";
-// import propagate from "./helpers/propagate.js";
-// import root_server from "./smart_root.js";
+import fmt from "./helpers/fmt.js"
 
 export async function main(ns) {
   switch (ns.args[0]) {
@@ -12,22 +8,19 @@ export async function main(ns) {
       recon(ns);
       break;
     case "weaken":
-      await weaken_server(ns, ns.args[1]);
+      ns.run('helpers/weaken_server.js', 1, ns.args[1]);
       break;
     case "hack":
-      await hack_server(ns, ns.args[1]);
+      ns.run('helpers/hack_server.js', 1, ns.args[1]);
       break;
     case "stats":
-      get_stats(ns, ns.args[1]);
+      ns.run('helpers/get_stats.js', 1, ns.args[1]);
       break;
     case "money":
       get_money(ns);
       break;
-    case "propagate":
-      propagate(ns);
-      break;
     case "nuke":
-      root_server(ns, ns.args[1]);
+      ns.run('smart_root.js', 1, ns.args[1]);
       break;
     default:
       ns.tprint("I need to know what you want me to do...");
@@ -45,13 +38,6 @@ function get_money(ns) {
 
   for (let server of ns.scan()) {
     server_funds = ns.getServerMoneyAvailable(server);
-    ns.tprint(
-      `${server}: \$${
-        Math.trunc(server_funds).toString().replace(
-          /\B(?=(\d{3})+(?!\d))/g,
-          ",",
-        )
-      }`,
-    );
+    ns.tprint(`${server}: \$${fmt(Math.trunc(server_funds))}`);
   }
 }
